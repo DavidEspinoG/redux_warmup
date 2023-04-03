796d6c0d58a506ca2809bf2c93a894aae029b24e
David Espino <davidespinog@gmail.com>
2023-04-03 12:00:52 -0600

    Add counter managed by redux

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterReducer } from "./slices/counter";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    
-  }
+    counter: counterReducer}
 })
+
+export { store };
